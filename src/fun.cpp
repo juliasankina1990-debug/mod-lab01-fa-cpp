94f0e7b93e24b336e88ebbeb03f4d316d2826d74
juliasankina1990-debug <juliasankina1990@gmail.com>
2026-04-21 07:03:15 +0300

    Implement faStr2: words starting with capital Latin letter, then lowercase

--- src/fun.cpp
+++ src/fun.cpp
@@ -26,7 +26,34 @@ unsigned int faStr1(const char *str) {
 }
 
 unsigned int faStr2(const char *str) {
-    return 0; // stub
+    if (!str) return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool good = true;
+    bool firstChar = true;
+
+    for (const char *p = str; *p; ++p) {
+        if (std::isspace(static_cast<unsigned char>(*p))) {
+            if (inWord && good) ++count;
+            inWord = false;
+            good = true;
+            firstChar = true;
+        } else {
+            if (!inWord) {
+                inWord = true;
+                good = true;
+                firstChar = true;
+            }
+            if (firstChar) {
+                if (!std::isupper(static_cast<unsigned char>(*p))) good = false;
+                firstChar = false;
+            } else {
+                if (!std::islower(static_cast<unsigned char>(*p))) good = false;
+            }
+        }
+    }
+    if (inWord && good) ++count;
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
